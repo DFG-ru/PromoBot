3ee29e116480e26187380cae67539866186648ba
DFG <dfg.gerasimov@yandex.ru>
2025-12-23 20:07:35 +0400

    Изменение истории посещений
    не вставил этот файл в предыдущий коммит

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -3,6 +3,7 @@ from aiogram import types
 from aiogram.types import Message
 from .keyboards import phone_button
 from datetime import datetime
+# from middlewares import userDatabase_funcs as userDB
 import logging
 
 # Константа для приветственного сообщения
@@ -11,11 +12,12 @@ WELCOME_MESSAGE = (
     "чтобы получить купон!"
 )
 
-async def start_handler(message: Message):
+async def start_handler(message: Message, bot_instance):
     """
     Обработчик команды /start.
     Отправляет приветственное сообщение с кнопкой, чтобы поделиться номером телефона.
     """
+
     user_id = message.from_user.id
     username = message.from_user.username or None
     full_name = message.from_user.full_name or None
@@ -25,10 +27,12 @@ async def start_handler(message: Message):
     logging.info(
         f"User logged in. | fullname={full_name}, username=@{username}, id={user_id}"
     )
+    # logging.info(f"New user authorized. | fullname={full_name}, username=@{username}, id={user_id}")
+    # userDB.write(user.id, user.first_name, user.last_name, user.username, contact.phone_number)
     # Отправка приветственного сообщения с кнопкой, чтобы поделиться номером телефона
     visit_history_path = Path().joinpath('data', 'VisitHistory.txt')
     with open(visit_history_path, 'a+') as used_codes:
-            used_codes.write(f"{last_active};{user_id};{username};{full_name}\n")  # добавляем код с новой строкой
+            used_codes.write(f"{last_active};{user_id};{username};{full_name};{bot_instance.botname}\n")  # добавляем код с новой строкой
     
     await message.answer(
         WELCOME_MESSAGE,
