2a1fdf934926749b04e8f818087abf05102a74d3
DFG <dfg.gerasimov@yandex.ru>
2025-12-23 19:35:03 +0400

    Добавлена история посещений

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -1,3 +1,4 @@
+from path import Path
 from aiogram import types
 from aiogram.types import Message
 from .keyboards import phone_button
@@ -24,8 +25,11 @@ async def start_handler(message: Message):
     logging.info(
         f"User logged in. | fullname={full_name}, username=@{username}, id={user_id}"
     )
-
     # Отправка приветственного сообщения с кнопкой, чтобы поделиться номером телефона
+    visit_history_path = Path().joinpath('data', 'VisitHistory.txt')
+    with open(visit_history_path, 'a+') as used_codes:
+            used_codes.write(f"{last_active};{user_id};{username};{full_name}\n")  # добавляем код с новой строкой
+    
     await message.answer(
         WELCOME_MESSAGE,
         reply_markup=phone_button
